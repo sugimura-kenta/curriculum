d4d731fbd8973af12ea6ee0390d8be77d402bc12
ramugisu <64357395+ramugisu@users.noreply.github.com>
2021-01-30 06:47:13 +0900

    課題提出　条件分岐・演算子

---
+++ JavaScript/1-3/main.js
@@ -0,0 +1,21 @@
+// 問１
+let a = 5;
+let b = 14;
+console.log(a !== 10);
+console.log(b >= 10 && b <= 20 && b % 2 === 0);
+
+//問２
+const x = 10;
+if(x >= 10 && x <= 20){
+    console.log("成功です");
+}else{
+    console.log("失敗です");
+}
+
+//問３
+const y = 0;
+if(y % 2 === 0){
+    console.log("偶数です");
+}else if (y % 2 !== 0){
+    console.log("奇数です");
+}
